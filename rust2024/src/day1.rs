eb66b950768e87910f6c07fcf17141f12aa021b5
Marco Coccato <coccato.m@gmail.com>
2024-12-01 11:50:02 +0100

    Added day1 program and data

--- rust2024/src/day1.rs
+++ rust2024/src/day1.rs
@@ -1,15 +1,55 @@
-use std::io::BufRead;
+use std::{collections::BinaryHeap, io::BufRead};
 
-use crate::utils;
+use crate::utils::create_buffer_reader;
 
 pub fn day1()
 {
-    let reader = utils::create_buffer_reader(String::from("./data/testData.txt"));
+    let reader = create_buffer_reader(String::from("./data/day1Data.txt"));
 
+    
+    // due binary heap da riempire
+    let mut left_buffer: BinaryHeap<i32> = BinaryHeap::new();
+    let mut right_buffer: BinaryHeap<i32> = BinaryHeap::new();
     for wrapped_line in reader.lines()
     {
         let line = wrapped_line.unwrap();
-        print!("{line}\n");
+        let split : Vec<&str> = line.split_whitespace().collect();
+
+        left_buffer.push(split[0].parse::<i32>().unwrap());
+        right_buffer.push(split[1].parse::<i32>().unwrap());
     }
 
+    let left = left_buffer.into_sorted_vec();
+    let right = right_buffer.into_sorted_vec();
+    let mut distance : i32 = 0;
+
+    for (li,ri) in left.iter().zip(right.iter())
+    {
+        distance += (li-ri).abs();
+    }
+
+    println!("Total distance {distance}");
+
+    let mut similarity = 0;
+    for li in left
+    {
+        similarity += li * count_occurrency(&li, &right);
+    }
+
+    println!("Total Similarity {similarity}");
+}
+
+fn count_occurrency(val : &i32, list : &Vec<i32>) -> i32
+{
+    let mut result = 0;
+
+    for item in list
+    {
+        if item == val
+        {
+            result += 1;
+        }
+    }
+
+    return result;
 }
